06c73a31d7a9c4a63051aae936d85846f422b7c2
Amalya <frauasse@gmail.com>
2023-10-11 13:02:21 +0200

    first commit

---
+++ Calc_chalange/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xamarin.Forms;
+
+namespace Calc_chalange
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            GenerateRows();
+        }
+
+        private void GenerateRows()
+        {
+            List<TaskRow> rows = new List<TaskRow>();
+
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int firstNumber = random.Next(1, 11);
+                int secondNumber = random.Next(1, 11);
+
+                rows.Add(new TaskRow { FirstNumber = firstNumber, SecondNumber = secondNumber });
+            }
+
+            taskListView.ItemsSource = rows;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Handle the button click event here if needed
+        }
+    }
+
+    public class TaskRow
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+    }
+
+
+
+
+
+
+
+}
+    
+
+    
+
